4bf8935322387fec02eaeca1996ae4a021b86835
Vishal <swamivishal14781516@gmail.com>
2024-12-23 06:01:37 +0530

    first commit

---
+++ code files/main.py
@@ -0,0 +1,29 @@
+print('Hello World!')
+
+var = 'hi, sir!'
+
+print(f'{var} Nice to See You!!' )
+
+var2 = input("Say your name:")
+
+print(f'Your input: {var2}')
+
+name = input('Enter Your name:' )
+destination = input('What is your dream place to visit?!:' )
+
+print(
+    f'Hi, {name}\nYou have won a Free Trip to {destination}!! Enjoy your vacations!! ;)'
+    )
+
+#Total students Calculator:
+
+std1 = int(input('Enter number of Male Students: '))
+std2 = int(input('Enter number of Female Students: '))
+std3 = int(input('Enter number of Trans Students: '))
+total_std = std1 + std2 + std3
+print(f'Total number of students: {total_std}')
+
+var3 = 5
+var3 %= 2
+print('This is "%=" useage result:',var3)
+
