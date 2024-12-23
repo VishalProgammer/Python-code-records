5be8c246d95110b16d7ee8709d47739ad9248bb3
Vishal <swamivishal14781516@gmail.com>
2024-12-23 06:36:19 +0530

    day 1 projects & code

--- Day 1/simple-sum-calc.py
+++ Day 1/simple-sum-calc.py
@@ -3,5 +3,6 @@
 std1 = int(input('Enter number of Male Students: '))
 std2 = int(input('Enter number of Female Students: '))
 std3 = int(input('Enter number of Trans Students: '))
-total_std = std1 + std2 + std3
+std4 = int(input('Enter number of Absent Students: ')) 
+total_std = std1 + std2 + std3 - std4
 print(f'Total number of students: {total_std}')
