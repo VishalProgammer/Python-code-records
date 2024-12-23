fe7cd1380f31c1d29d9485c268e4535a1fd97a5d
Vishal <swamivishal14781516@gmail.com>
2024-12-23 06:34:33 +0530

    day 1 projects & code

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -15,14 +15,6 @@ print(
     f'Hi, {name}\nYou have won a Free Trip to {destination}!! Enjoy your vacations!! ;)'
     )
 
-#Total students Calculator:
-
-std1 = int(input('Enter number of Male Students: '))
-std2 = int(input('Enter number of Female Students: '))
-std3 = int(input('Enter number of Trans Students: '))
-total_std = std1 + std2 + std3
-print(f'Total number of students: {total_std}')
-
 var3 = 5
 var3 %= 2
 print('This is "%=" useage result:',var3)
---
+++ Day 1/simple-sum-calc.py
@@ -0,0 +1,7 @@
+#Total students Calculator:
+
+std1 = int(input('Enter number of Male Students: '))
+std2 = int(input('Enter number of Female Students: '))
+std3 = int(input('Enter number of Trans Students: '))
+total_std = std1 + std2 + std3
+print(f'Total number of students: {total_std}')
