ad8f7ccd2e1099ad0292bf0c5967ffd09a8bdd92
Vishal <swamivishal14781516@gmail.com>
2024-12-23 06:25:10 +0530

    day 1 project

---
+++ Day 1/circumference-finder.py
@@ -0,0 +1,9 @@
+import math
+
+#Find Circumference of Circle:
+
+r  = float(input('Enter the radius of the circle: '))
+
+circumference = 2 * math.pi * r
+
+print(f'Circumference of the Circle is: {circumference}')
