bfd83a242418f25fa09bd46a043452fa1e61f861
Vishal <swamivishal14781516@gmail.com>
2024-12-31 12:53:32 +0530

    day 3 projects & code

---
+++ Day 3/code_dump.py
@@ -0,0 +1,18 @@
+import time
+
+while True:
+    age = int(input('age of your dog:'))
+    if age == 0:
+        print('Your Dog is not even born yet?!')
+        print('Enter a valid age!')
+    elif age < 0:
+        print('Enter a valid age!')
+    else:
+        break
+
+print(f'Your Dog is {age} Years Old!!')
+
+for i in reversed(range(1,11)):
+    print(i)
+    time.sleep(1)
+print('FUCK YOU!!')
